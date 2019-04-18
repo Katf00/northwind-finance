8b93b94d75fde94d5765421cc019722bb351b5a4
Paola <katherineblandon2000@gmail.com>
2019-04-17 23:45:39 -0500

    Interfaz original del proyecto de finanzas

---
+++ northwind-finance/northwind-finance/Vista/Operacion_Compra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace northwind_finance.Vista
+{
+    public partial class Operacion_Compra : Form
+    {
+        public Operacion_Compra()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
